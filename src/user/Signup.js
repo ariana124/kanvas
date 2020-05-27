a7c146061b74c92a9d02ab5fb53fffcf1ea282c0
ariana124 <ariana.bibiano@gmail.com>
2020-05-27 15:15:27 -0700

    refactored code: moved form tags into its own method

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -60,6 +60,24 @@ class Signup extends Component {
             .catch(err => console.log(err))
     }
 
+    signupForm = (name, email, password) => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input onChange={this.handleChange("name")} className="form-control" type="text" value={name}/>
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Email</label>
+                <input onChange={this.handleChange("email")} className="form-control" type="email" value={email}/>
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Password</label>
+                <input onChange={this.handleChange("password")} className="form-control" type="password" value={password}/>
+            </div>
+            <button onClick={this.clickSubmit} className="btn btn-raised btn-primary">Submit</button>
+        </form> 
+    )
+
     render () {
         const {name, email, password, error, open} = this.state;
         return (
@@ -67,27 +85,13 @@ class Signup extends Component {
                 <h2 className="mt-5 mb-5">Signup</h2>
 
                 {/* This div will display error messages if there are issues when the user is signing up. */}
-                <div className="alert alert-primary" style={{ display: error ? "" :" none" }}>{error}</div>
+                <div className="alert alert-danger" style={{ display: error ? "" :" none" }}>{error}</div>
 
                 <div className="alert alert-info" style={{ display: open ? "" :" none" }}>
                     New account is successfully created. Please sign in.
                 </div>
 
-                <form>
-                    <div className="form-group">
-                        <label className="text-muted">Name</label>
-                        <input onChange={this.handleChange("name")} className="form-control" type="text" value={name}/>
-                    </div>
-                    <div className="form-group">
-                        <label className="text-muted">Email</label>
-                        <input onChange={this.handleChange("email")} className="form-control" type="email" value={email}/>
-                    </div>
-                    <div className="form-group">
-                        <label className="text-muted">Password</label>
-                        <input onChange={this.handleChange("password")} className="form-control" type="password" value={password}/>
-                    </div>
-                    <button onClick={this.clickSubmit} className="btn btn-raised btn-primary">Submit</button>
-                </form>
+                {this.signupForm(name, email, password)}
             </div>
         );
     }
