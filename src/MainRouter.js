32c6d4a4277c864ebcc3d1104771c0a50af7a6ee
ariana124 <ariana.bibiano@gmail.com>
2020-05-28 15:04:17 -0700

    added Menu navigation component

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './core/Home';
+import Menu from './core/Menu';
 import Signup from './user/Signup';
 import Signin from './user/Signin';
 
 
 const MainRouter = () => (
     <div>
+        <Menu />
         {/* This allows for switching between components when we navigate to different URLs. */}
         <Switch>
             <Route exact path="/" component={Home}/>
