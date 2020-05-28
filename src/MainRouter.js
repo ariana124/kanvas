3ec8863d87daa9775e8b2c667d9d9a9be73f539b
ariana124 <ariana.bibiano@gmail.com>
2020-05-27 18:07:48 -0700

    added new route for signin

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './core/Home';
 import Signup from './user/Signup';
+import Signin from './user/Signin';
 
 
 const MainRouter = () => (
@@ -10,6 +11,7 @@ const MainRouter = () => (
         <Switch>
             <Route exact path="/" component={Home}/>
             <Route exact path="/signup" component={Signup}/>
+            <Route exact path="/signin" component={Signin}/>
         </Switch>
     </div>
 )
