ccd64bfd2d1c18fbbd15946e343a00ad0ffc7bfa
ariana124 <ariana.bibiano@gmail.com>
2020-05-28 15:05:22 -0700

    added comment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import MainRouter from './MainRouter';
 
+// The MainRouter is wrapped within the BrowserRouter letting us switch between components without having to reload/refresh the browser.
 const App = () => (
   <BrowserRouter>
     <MainRouter />
