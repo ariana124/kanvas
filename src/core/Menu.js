9ebef219ae23d7264c89a89b938a06eed841240e
ariana124 <ariana.bibiano@gmail.com>
2020-05-28 21:04:00 -0700

    implemented user sign out

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -7,6 +7,22 @@ const isActive = (history, path) => {
         else return { color: "#fff" };
 }
 
+
+// We use next(a method) and another callback to redirect the user to another page such as login or home when they sign out.
+export const signout = (next) => {
+    if (typeof window !== "undefined") localStorage.removeItem("jwt");
+    next();
+    // Will eventually change to environment variable.
+    return fetch("http://local:8080/signout", {
+        method: "GET"
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+}
+
+
 // This is a functional component, not a class component since it doesn't need to have a state.
 const Menu = ({history}) => (
     <div>
@@ -20,6 +36,10 @@ const Menu = ({history}) => (
             </li>
             <li className="nav-item">
                 <Link className="nav-link" style={isActive(history, "/signup")} to="/signup">Sign Up</Link>
+            </li>
+            <li className="nav-item">
+                {/* We use the a tag because we're not trying to navigate the user to another component. */}
+                <a href className="nav-link" style={isActive(history, "/signup"), {cursor: "pointer"}} onClick={() => signout(() => history.push('/'))}>Sign Out</a>
             </li>
         </ul>
     </div>
