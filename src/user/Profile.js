e3fddb8a725e1baab91573604114b1466e540e9e
ariana124 <ariana.bibiano@gmail.com>
2020-06-02 19:05:34 -0700

    created api request to get the user's info at signin

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { isAuthenticated } from '../auth';
+import { Redirect } from 'react-router-dom';
 
 
 class Profile extends Component {
@@ -11,17 +12,42 @@ class Profile extends Component {
         }
     }
 
-    // A lifecycle method (?), when this component mounts we get the userId to make a GET request later in the backend.
+    // A lifecycle method (?), when this component mounts we get the userId to make a GET request to the backend.
     componentDidMount() {
-        console.log("userid from route params:", this.props.match.params.userId);
-    }
+        const userId = this.props.match.params.userId;
+        fetch (`${process.env.REACT_APP_API_URL}/api/user/${userId}`, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                "Content-type": "application/json",
+                Authorization: `Bearer ${isAuthenticated().token}`
+            }
+        })
+            // This returns the json response with the user's information.
+            .then(response => {
+                return response.json();
+            })
+
+            // This redirects the user to the signin component if they're not authenticated.
+            .then(data => {
+                if (data.error) {
+                    this.setState({ redirectToSignin: true })
+                } else {
+                    this.setState({ user: data });
+                }
+            })
+    };
 
     render() {
+        const redirectToSignin = this.state.redirectToSignin;
+        if (redirectToSignin) return <Redirect to="/signin "/>;
+
         return (
             <div className="container">
                 <h2 className="mt-5 mb-5">Profile</h2>
                     <p> Hello {isAuthenticated().user.name}</p>
                     <p> Email: {isAuthenticated().user.email}</p>
+                    <p>{`Joined ${new Date(this.state.user.created).toDateString()}`}</p>
             </div>
         )
     }
