eda7a8fdb08feb5dead604adc71f408395c346fe
ariana124 <ariana.bibiano@gmail.com>
2020-06-03 16:00:27 -0700

    relocated delete profile button to this DeleteUser component

---
+++ src/user/DeleteUser.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DeleteUser extends Component {
+    render() {
+        return (
+            <button className="btn btn-raised btn-danger">
+                Delete Profile
+            </button>
+        );
+    }
+}
+
+export default DeleteUser;
