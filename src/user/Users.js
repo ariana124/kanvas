496f0d34f9fcc9edd449e5891783dddba780bac9
ariana124 <ariana.bibiano@gmail.com>
2020-06-03 13:47:39 -0700

    updated Users component to display just usernames

--- src/user/Users.js
+++ src/user/Users.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import { list } from './apiUser';
 
 
 class Users extends Component {
+    constructor() {
+        super()
+        this.state = {
+            users: []
+        }
+    }
+
+    componentDidMount() {
+        list().then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                this.setState({ users: data });
+            }
+        })
+    }
+
     render() {
+        const {users} = this.state;
         return (
             <div className="container">
                 <h2 className="mt-5 mb-5">Users</h2>
+
+                <div className="card">
+                    {/* This takes each user name and displays it on the user page. Will have to update the response nodeapi
+                    /controllers/users.js allUsers method once KP merges the backend.*/}
+                    {users.map((user, i) => (
+                        <div key={i}>
+                            <p>{user.name}</p>
+                        </div>
+                    ))}
+                </div>
             </div>
         )
     }
