5540a6e8321b67f427bb7e08ee3bc1d101bcd878
ariana124 <ariana.bibiano@gmail.com>
2020-06-02 21:18:11 -0700

    created Users component

---
+++ src/user/Users.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Users extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h2 className="mt-5 mb-5">Users</h2>
+            </div>
+        )
+    }
+}
+
+export default Users;
