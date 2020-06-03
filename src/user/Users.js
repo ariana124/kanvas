df4a36f6dda7b6608c43f2742136f54b4151d8de
ariana124 <ariana.bibiano@gmail.com>
2020-06-03 14:20:55 -0700

    created user cards

--- src/user/Users.js
+++ src/user/Users.js
@@ -20,21 +20,29 @@ class Users extends Component {
         })
     }
 
+    renderUsers = (users) => (
+        <div className="row">
+            {/* This takes each user name and displays it on the user page. Will have to update the response nodeapi
+            /controllers/users.js allUsers method once KP merges the backend.*/}
+            {users.map((user, i) => (
+                <div className="card col-md-4" key={i}>
+                    <img className="card-img-top" src="" alt="User card"/>
+                    <div className="card-body">
+                        <h5 className="card-title">{user.name}</h5>
+                        <p className="card-text">The user's bio or short self intro would go here.</p>
+                        <a href="#" className="btn btn-raised btn-sm btn-primary">View Profile</a>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+
     render() {
         const {users} = this.state;
         return (
             <div className="container">
                 <h2 className="mt-5 mb-5">Users</h2>
-
-                <div className="card">
-                    {/* This takes each user name and displays it on the user page. Will have to update the response nodeapi
-                    /controllers/users.js allUsers method once KP merges the backend.*/}
-                    {users.map((user, i) => (
-                        <div key={i}>
-                            <p>{user.name}</p>
-                        </div>
-                    ))}
-                </div>
+                {this.renderUsers(users)}
             </div>
         )
     }
