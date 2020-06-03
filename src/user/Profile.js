e6fcfe8f6e56ee3c7fe486af07e0308854fea3f2
ariana124 <ariana.bibiano@gmail.com>
2020-06-03 16:47:31 -0700

    added userId to delete the current logged in user

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -69,7 +69,7 @@ class Profile extends Component {
                                 <Link to={`/user/edit/${user._id}`} className="btn btn-raised btn-success mr-5">
                                     Edit Profile
                                 </Link>
-                                <DeleteUser />
+                                <DeleteUser userId={user._id} />
                             </div>
                         )}
                     </div>
