6b405942dc6e2c58642b7ae700e0109e5cd7727f
ariana124 <ariana.bibiano@gmail.com>
2020-06-04 14:59:31 -0700

    created update method

--- src/user/apiUser.js
+++ src/user/apiUser.js
@@ -1,5 +1,5 @@
 export const read = (userId, token) => {
-    return fetch (`${process.env.REACT_APP_API_URL}/api/user/${userId}`, {
+    return fetch (`${process.env.REACT_APP_API_URL}/user/${userId}`, {
         method: "GET",
         headers: {
             Accept: "application/json",
@@ -14,8 +14,25 @@ export const read = (userId, token) => {
         .catch(err => console.log(err));
 };
 
+export const update = (userId, token, user) => {
+    console.log("USER DATA UPDATE: ", user);
+    return fetch (`${process.env.REACT_APP_API_URL}/user/${userId}`, {
+        method: "PUT",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(user)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
 export const remove = (userId, token) => {
-    return fetch (`${process.env.REACT_APP_API_URL}/api/user/${userId}`, {
+    return fetch (`${process.env.REACT_APP_API_URL}/user/${userId}`, {
         method: "DELETE",
         headers: {
             Accept: "application/json",
@@ -31,7 +48,7 @@ export const remove = (userId, token) => {
 };
 
 export const list = () => {
-    return fetch (`${process.env.REACT_APP_API_URL}/api/users`, {
+    return fetch (`${process.env.REACT_APP_API_URL}/users`, {
         method: "GET"
     })
         // This returns the json response with the user's information.
