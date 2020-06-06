6d02e1cf1d1511fe2b2a262a35fc73b043adfcd0
Kevin Apostol <apostolkevin@gmail.com>
2020-06-06 15:49:45 -0700

    Adding images to posts to see at the homepage

--- src/post/Posts.js
+++ src/post/Posts.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { list } from './apiPost';
-// import DefaultProfile from '../images/profilepic.jpg';
+import DefaultPost from '../images/castle.jpg';
 
 
 class Posts extends Component {
@@ -32,6 +32,12 @@ class Posts extends Component {
                 return (
                     <div className="card col-md-4" key={i}>
                     <div className="card-body">
+                        <img
+                            src={`${process.env.REACT_APP_API_URL}/post/photo/${post._id}`}
+                            alt={post.title} onError={i => i.target.src = `${DefaultPost}`}
+                            className="img-thumbnail mb-3"
+                            style={{ height: "200px", width: "auto" }}
+                        />
                         <h5 className="card-title">{post.title}</h5>
                         <p className="card-text">{post.body.substring(0, 100)}</p>
                         <br/>
