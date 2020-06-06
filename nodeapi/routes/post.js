7452502015f57fecd2c96f63f8faff437fd476f1
Kevin Apostol <apostolkevin@gmail.com>
2020-06-06 15:36:43 -0700

    Added route for getting the photo

--- nodeapi/routes/post.js
+++ nodeapi/routes/post.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {getPosts, createPost, postbyUser, postById, isPoster, deletePost, updatePost} = require('../controllers/post')
+const {getPosts, createPost, postbyUser, postById, isPoster, deletePost, updatePost, photo} = require('../controllers/post')
 const {requireSignin} = require('../controllers/auth')
 const {userById} = require('../controllers/user')
 const {createPostValidator} = require('../validator')
@@ -10,6 +10,9 @@ router.post('/post/new/:userId', requireSignin, createPost, createPostValidator)
 router.get('/posts/by/:userId', postbyUser)
 router.put('/post/:postId', requireSignin, isPoster, updatePost)
 router.delete('/post/:postId', requireSignin, isPoster, deletePost)
+
+// Route to show photos of a post
+router.get('/post/photo/:postId', photo)
 
 // Any route containing :userId, our app will first execute userById()
 router.param('userId', userById)
