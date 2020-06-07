8ff9e0aac90e05d5a2b02c398af75887b7c585c8
Kevin Apostol <apostolkevin@gmail.com>
2020-06-06 23:08:51 -0700

    Added remove method to apiPost

--- src/post/apiPost.js
+++ src/post/apiPost.js
@@ -57,3 +57,19 @@ export const singlePost = (postId) => {
 //       .catch((err) => console.log(err))
 //   );
 // };
+
+export const remove = (postId, token) => {
+  return fetch (`${process.env.REACT_APP_API_URL}/post/${postId}`, {
+      method: "DELETE",
+      headers: {
+          Accept: "application/json",
+          "Content-type": "application/json",
+          Authorization: `Bearer ${token}`
+      }
+  })
+      // This returns the json response with the user's information.
+      .then(response => {
+          return response.json();
+      })
+      .catch(err => console.log(err));
+};
