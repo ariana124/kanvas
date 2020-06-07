31170d3a0e0601d0c77f8671dc50a5fab0506c34
Kevin Apostol <apostolkevin@gmail.com>
2020-06-06 17:26:53 -0700

    Added singlePost method on the API

--- nodeapi/controllers/post.js
+++ nodeapi/controllers/post.js
@@ -116,3 +116,7 @@ exports.photo = (req, res, next) => {
   res.set('Content-Type', req.post.photo.contentType)
   return res.send(req.post.photo.data)
 }
+
+exports.singlePost = (req, res) => {
+  return res.json(req.post)
+}
