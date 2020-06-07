e436e1a541454ae2886d1f685c9982204abe7cf9
Kevin Apostol <apostolkevin@gmail.com>
2020-06-06 17:29:35 -0700

    Added componentDidMount lifecycle

--- src/post/SinglePost.js
+++ src/post/SinglePost.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import { singlePost } from './apiPost'
 
 class SinglePost extends Component {
+    state = {
+        post: ''
+    }
+
+    componentDidMount = () => {
+        const postId = this.props.match.params.postId
+        singlePost(postId).then(data => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                this.setState({post: data})
+            }
+        })
+    }
+
     render() {
         return (
             <div>
                 <h2>single post</h2>
-                {this.props.match.params.postId}
+                {JSON.stringify(this.state.post)}
             </div>
 
         )
