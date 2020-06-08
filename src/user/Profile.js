2e0282c6865f51bce685051876fd43e4e2861244
ecaoili24 <erika.caoili@gmail.com>
2020-06-08 11:57:05 -0700

    Implemented the follow component and the follow/unfollow functionality with the backend

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -4,17 +4,44 @@ import { Redirect, Link } from 'react-router-dom';
 import { read } from './apiUser';
 import DefaultProfile from '../images/profilepic.jpg';
 import DeleteUser from './DeleteUser';
+import FollowProfileButton from './FollowProfileButton'
 
 
 class Profile extends Component {
     constructor() {
         super()
         this.state= {
-            user: "",
+            user: { following: [], followers: [] },
             redirectToSignin: false
+            following: false
+            error: ''
         };
     }
 
+    // a method to check follow
+    checkFollow = user => {
+        const jwt = isAuthenticated()
+        const match = user.followers.find(follower => {
+        // one id has many other ids (followers) and vice versa
+            return follower._id === jwt.user._id
+        })
+        return match
+    }
+
+    clickFollowButton = callApi => {
+        const userId = isAuthenticated().user._id;
+        const token = isAuthenticated().token;
+
+        callApi(userId, token, this.state.user._id)
+        .then(data => {
+            if(data.error) {
+                this.setState({error: data.error})
+            } else {
+                this.setState({user: data, following: !this.state.following})
+            }
+        })
+    }
+        
     init = (userId) => {
         const token = isAuthenticated().token;
         read(userId, token).then(data => {
@@ -22,7 +49,8 @@ class Profile extends Component {
             if (data.error) {
                 this.setState({ redirectToSignin: true })
             } else {
-                this.setState({ user: data });
+                let following = this this.checkFollow(data)
+                this.setState({ user: data, following });
             }
         })
     };
@@ -60,14 +88,14 @@ class Profile extends Component {
                             alt={user.name}
                         />
                     </div>
-                    <div className="col-md-6">
+                    < className="col-md-6">
                         <div className="lead mt-2">
                             <p> Hello {user.name}</p>
                             <p> Email: {user.email}</p>
                             <p>{`Joined ${new Date(user.created).toDateString()}`}</p>
                         </div>
                         {/* This checks if the user is authenticated and it matches the current user it will display the edit and delete buttons. */}
-                        {isAuthenticated().user && isAuthenticated().user._id === user._id && (
+                        {isAuthenticated().user ? isAuthenticated().user._id === user._id && (
                             <div className="d-inline-block mt-5">
                                 {/* Might need to add the /api route later. */}
                                 <Link to={`/user/edit/${user._id}`} className="btn btn-raised btn-success mr-5">
@@ -75,9 +103,11 @@ class Profile extends Component {
                                 </Link>
                                 <DeleteUser userId={user._id} />
                             </div>
+                        ) : (
+                            <FollowProfileButton following={this.state.following}
+                                onButtonClick={this.clickFollowButton} />
                         )}
                     </div>
-                </div>
 
                 <div className="row">
                     <div className="col md-12 mt-4">
