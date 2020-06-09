8696b7e456e0080ea7cb3bb5e23c0642a5d6699a
ariana124 <ariana.bibiano@gmail.com>
2020-06-09 10:34:39 -0700

    moved the follow and unfollow routes to the top

--- nodeapi/routes/user.js
+++ nodeapi/routes/user.js
@@ -14,15 +14,17 @@ const {
 } = require('../controllers/user')
 const {requireSignin} = require('../controllers/auth')
 
+// Route for following and follower list, it has to be in this specific order.
+router.put('/user/follow', requireSignin, addFollowing, addFollower)
+router.put('/user/unfollow', requireSignin, removeFollowing, removeFollower)
+
 router.get('/users', allUsers)
 router.get('/user/:userId', requireSignin, getUser)
 router.put('/user/:userId', requireSignin, updateUser)
 router.delete('/user/:userId', requireSignin, deleteUser)
+
 // Route to show user profile picture.
 router.get('/user/photo/:userId', userPhoto)
-// Route for following and follower list, it has to be in this specific order.
-router.put('/user/follow', requireSignin, addFollowing, addFollower)
-router.put('/user/unfollow', requireSignin, removeFollowing, removeFollower)
 
 // Any routes containing :userId, our app will first execute userById()
 // adds profile object in req with user info
