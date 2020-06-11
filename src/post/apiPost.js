a5262486c35ec27b5fd4b3cc1536a0c0f88215f3
Kevin Apostol <apostolkevin@gmail.com>
2020-06-10 18:43:47 -0700

    created listByUser method

--- src/post/apiPost.js
+++ src/post/apiPost.js
@@ -40,23 +40,20 @@ export const singlePost = (postId) => {
   );
 };
 
-// export const listByUser = (userId) => {
-//   return (
-//     fetch(`${process.env.REACT_APP_API_URL}/post/by/${userId}`, {
-//       method: "GET",
-//       headers: {
-//         Accept: "application/json",
-//         'Content-Type': "application/json",
-//         Authorization: `Bearer ${token}`,
-//       }
-//     })
-//       // This returns the json response with the user's information.
-//       .then((response) => {
-//         return response.json();
-//       })
-//       .catch((err) => console.log(err))
-//   );
-// };
+export const listByUser = (userId, token) => {
+  return fetch(`${process.env.REACT_APP_API_URL}/posts/by/${userId}`, {
+      method: "GET",
+      headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+      }
+  })
+      .then(response => {
+          return response.json();
+      })
+      .catch(err => console.log(err));
+};
 
 export const remove = (postId, token) => {
   return fetch (`${process.env.REACT_APP_API_URL}/post/${postId}`, {
