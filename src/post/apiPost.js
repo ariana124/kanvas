ddedf902872105f22c0e77e4a0ce2ab9a0293e8f
ariana124 <ariana.bibiano@gmail.com>
2020-06-11 13:58:16 -0700

    created comment and uncomment methods

--- src/post/apiPost.js
+++ src/post/apiPost.js
@@ -118,3 +118,37 @@ export const unlike = (userId, token, postId) => {
       })
       .catch(err => console.log(err))
 }
+
+// Comment and uncomment
+export const comment = (userId, token, postId, comment) => {
+  return fetch (`${process.env.REACT_APP_API_URL}/post/comment`, {
+      method: "PUT",
+      headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+      },
+      body: JSON.stringify({userId, postId, comment})
+  })
+      .then(response => {
+          return response.json()
+      })
+      .catch(err => console.log(err))
+}
+
+export const uncomment = (userId, token, postId, comment) => {
+  return fetch (`${process.env.REACT_APP_API_URL}/post/uncomment`, {
+      method: "PUT",
+      headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+      },
+      // We need the comment id to remove the correct comment.
+      body: JSON.stringify({userId, postId, comment})
+  })
+      .then(response => {
+          return response.json()
+      })
+      .catch(err => console.log(err))
+}
