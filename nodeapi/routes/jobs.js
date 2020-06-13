277c12d8109088a4f1a51b7594fa30f2830cc6a7
Kevin Apostol <apostolkevin@gmail.com>
2020-06-12 19:13:05 -0700

    Router

--- nodeapi/routes/jobs.js
+++ nodeapi/routes/jobs.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const {getJobs} = require('../controllers/jobs')
 
-router.get('/posts', getJobs)
+router.get('/jobs', getJobs)
 
-module.exports.router
+module.exports = router
