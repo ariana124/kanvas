92dcd6176dca8164ae84070686ffcd2a84575755
ariana124 <ariana.bibiano@gmail.com>
2020-06-13 16:20:57 -0700

    fixed styling

--- src/user/FindPeople.js
+++ src/user/FindPeople.js
@@ -55,7 +55,7 @@ class FindPeople extends Component {
                     {/* If there's an error when displaying the user's profile image, it displays the default image instead. */}
                     <img 
                         className="img-thumbnail"
-                        style={{height: "350px", width: "auto"}}
+                        style={{height: "300px", width: "300px"}}
                         src={`${process.env.REACT_APP_API_URL}/user/photo/${user._id}`}
                         onError={i => (i.target.src = `${DefaultProfile}`)}
                         alt={user.name}
