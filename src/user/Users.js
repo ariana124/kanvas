0ecffccbdb1b4ea728a163e1115844a18744f687
ariana124 <ariana.bibiano@gmail.com>
2020-06-13 15:33:13 -0700

    changed classnames and styling

--- src/user/Users.js
+++ src/user/Users.js
@@ -32,7 +32,7 @@ class Users extends Component {
                     {/* If there's an error when displaying the user's profile image, it displays the default image instead. */}
                     <img 
                         className="img-thumbnail"
-                        style={{height: "350px", width: "auto"}}
+                        style={{height: "250px", width: "250px"}}
                         src={`${process.env.REACT_APP_API_URL}/user/photo/${user._id}`}
                         onError={i => (i.target.src = `${DefaultProfile}`)}
                         alt={user.name}
