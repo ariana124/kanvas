79406bc9dddebcbd121cdd86c5b4e00a77511f99
Kevin Apostol <apostolkevin@gmail.com>
2020-06-14 14:20:28 -0700

    Added JobList functional component

---
+++ src/jobs/JobList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import '../styling/jobList.scss';
+
+export default class JobList extends Component {
+    render() {
+        return (
+                <div className="row">
+                    <a class="list-group-item col">
+                    <div class="bmd-list-group-col">
+                        <p class="list-group-item-heading">{this.props.job.title}</p>
+                        <p class="list-group-item-text">Company: {this.props.job.company}</p>
+                        <p class="list-group-item-text">Type: {this.props.job.type}</p>
+                    </div>
+                    </a>
+                    <a className="job_info" href={this.props.job.url} target="_blank">Info</a>
+                </div>
+        )
+    }
+}
