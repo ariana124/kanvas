f88b3ea0f55c78a42415b588c32eadd95084f6de
ariana124 <ariana.bibiano@gmail.com>
2020-06-15 11:34:15 -0700

    fixed spacing

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Posts from '../post/Posts'
+import Posts from '../post/Posts';
+
+
 const Home = () => (
     <div>
         <div className="jumbotron">
             <h2>Home</h2>
-            <p className="lead">Welcome to React Frontend</p>
+            <p className="lead">Welcome to Kanvas</p>
         </div>
         <div className="container">
             <Posts />
