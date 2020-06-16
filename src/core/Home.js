eb76d1fba3cd71f202edbbdfc118427fb17af54e
ariana124 <ariana.bibiano@gmail.com>
2020-06-15 18:00:48 -0700

    centered jumbotron text, will change tagline later

--- src/core/Home.js
+++ src/core/Home.js
@@ -4,9 +4,15 @@ import Posts from '../post/Posts';
 
 const Home = () => (
     <div>
-        <div className="jumbotron">
-            <h2>Home</h2>
-            <p className="lead">Welcome to Kanvas</p>
+        <div className="jumbotron text-center">
+            <h2>Welcome to Kanvas</h2>
+            <br/>
+            <p style={{ fontFamily: `sans serif` }}>
+                <i>
+                    Picture your career like a painting, not a ladder. <br/>
+                    It's something that takes time and patience.
+                </i>
+            </p>
         </div>
         <div className="container">
             <Posts />
